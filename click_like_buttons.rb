09725533ec88e74a4f3174d2a7868b52c1944379
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-07-07 15:13:28 +0900

    なんか無限にいいね！押せるやつ

---
+++ click_like_buttons.rb
@@ -0,0 +1,55 @@
+require 'open-uri'
+require 'json'
+require 'selenium-webdriver'
+require 'headless'
+
+headless = Headless.new
+headless.start
+
+driver = Selenium::WebDriver.for :chrome
+
+driver.navigate.to 'https://jp.pinterest.com/'
+
+p driver.title
+
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko1.png")
+
+driver.find_element(:xpath, "//div[@class='inspiredSearchDialog ']/button").click
+
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko1.png")
+
+username = driver.find_element(:xpath, "//li[@class='loginUsername']/input")
+username.send_keys('i.am.eager.for.peace@gmail.com')
+
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko2.png")
+
+username = driver.find_element(:xpath, "//li[@class='loginPassword']/input")
+username.send_keys('19924128takahiro')
+
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko3.png")
+
+driver.find_element(:xpath, "//div[@class='formFooterButtons']/button").click
+
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko3.png")
+
+p driver.title
+
+driver.navigate.to 'https://jp.pinterest.com/search/pins/?q=chanel'
+
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko3.png")
+
+driver.find_element(:xpath, "//div[@class='GridItems Module centeredWithinWrapper padItems variableHeightLayout']/div[4]").click
+
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko3.png")
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko3.png")
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko3.png")
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko3.png")
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko3.png")
+
+driver.find_element(:xpath, "//div[@class='repinLike']/button[3]").click
+
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko4.png")
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko4.png")
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko4.png")
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko4.png")
+driver.save_screenshot("/home/vagrant/dev/#{Time.now.to_s}_unko4.png")
